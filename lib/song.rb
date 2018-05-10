371ec12673472b276992e05b8fc0458287eac0cb
David Chiu <davidwchiu@gmail.com>
2018-05-10 23:54:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -12,54 +14,55 @@ class Song
 
   def self.create
     song = self.new
-    song.save
-    song
+    @@all << song
+    return song
   end
 
   def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song.save
-    song
+   song = self.new
+   song.name = name
+   return song
   end
 
   def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    song.save
-    song
+   song = self.new
+   song.name = name
+   @@all << song
+   return song
   end
 
   def self.find_by_name(name)
-    @@all.detect{ |song| song.name == name}
-end
-
+    Song.all.detect {|song| song.name == "#{name}"}
+  end
+  
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
+    if song = self.find_by_name(name)
+      return song
     else
-      self.create_by_name(name)
+      Song.create_by_name(name)
     end
   end
-
+  
   def self.alphabetical
-    @@all.sort_by{ |song| song.name }
+    self.all.sort_by {|word| word.name} 
+  end
+  
+  def self.new_from_filename(mp3)
+    array = mp3.split(/[-.]/).map(&:strip)
+    array.pop
+    song = self.new
+    song.name = array[1]
+    song.artist_name = array[0]
+    return song
+  end
+  
+  def self.create_from_filename(mp3)
+      song = self.new_from_filename(mp3)
+      song.save
   end
 
-  def self.new_from_filename(song)
-     song_info = song.gsub(".mp3","").split(" - ")
-     song = self.new_by_name(song_info[1])
-     song.artist_name = song_info[0]
-     song
-   end
+  def self.destroy_all
+    @@all.clear
+  end
 
-   def self.create_from_filename(song)
-     song_info = song.gsub(".mp3","").split(" - ")
-     song = self.create_by_name(song_info[1])
-     song.artist_name = song_info[0]
-
-   end
-
-   def self.destroy_all
-     @@all.clear
- end
+end
