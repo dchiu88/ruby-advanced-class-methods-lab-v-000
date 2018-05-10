cff482e92ce3b2e2e9e350c0c12df3ed51eade3b
David Chiu <davidwchiu@gmail.com>
2018-05-10 23:02:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,5 +28,14 @@ class Song
     song.name = name
     song.save
     song
+  end
 
+  def self.find_by_name(name)
+    @@all.detect{ |song| song.name == name}
 end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
